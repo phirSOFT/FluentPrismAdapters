41ee1cf01f010546b0bd2116e3bcd2bff603aa11
Philemon Eichin <phileicht@gmx.de>
2018-01-22 15:50:05 +0100

    Add RibbonRegionAdapter

---
+++ FluentPrismRegionAdapter/phirSOFT.FluentPrismAdapters/RibbonRegionAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using Fluent;
+using Prism.Regions;
+
+namespace phirSOFT.FluentPrismAdapters
+{
+    public class RibbonRegionAdapter : TopologicalSortedRegionAdapterBase<Ribbon>
+    {
+        protected RibbonRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory) : base(regionBehaviorFactory)
+        {
+        }
+
+        protected override IList GetItemCollection(Ribbon container)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IList GetItemCollection(Ribbon container, DependencyObject item)
+        {
+            switch (item)
+            {
+                case RibbonTabItem _:
+                    return container.Tabs;
+                case QuickAccessMenuItem _:
+                    return container.QuickAccessItems;
+                case RibbonContextualTabGroup _:
+                    return container.ContextualGroups;
+                case UIElement _:
+                    return container.ToolBarItems;
+            }
+            return null;
+        }
+    }
+}
