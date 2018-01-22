3f129d2f1243bfbfba1157f6261bdf0051dfa0bd
Philemon Eichin <phileicht@gmx.de>
2018-01-22 16:15:44 +0100

    Add MenuRegionAdapters
    * RbbonMenu
    * Backstage (BackstageTabControl)

---
+++ FluentPrismRegionAdapter/phirSOFT.FluentPrismAdapters/BackstageTabControlRegionAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using Fluent;
+using Prism.Regions;
+
+namespace phirSOFT.FluentPrismAdapters
+{
+    public class BackstageTabControlRegionAdapter : TopologicalSortedRegionAdapterBase<BackstageTabControl>
+    {
+        public BackstageTabControlRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory) : base(regionBehaviorFactory)
+        {
+        }
+
+        protected override IList GetItemCollection(BackstageTabControl container)
+        {
+            return container.Items;
+        }
+    }
+}
---
+++ FluentPrismRegionAdapter/phirSOFT.FluentPrismAdapters/RibbonMenuRegionAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Windows;
+using Fluent;
+using Prism.Regions;
+
+namespace phirSOFT.FluentPrismAdapters
+{
+    public class RibbonMenuRegionAdapter : TopologicalSortedRegionAdapterBase<RibbonMenu>
+    {
+        public RibbonMenuRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory) : base(regionBehaviorFactory)
+        {
+        }
+
+        protected override IList GetItemCollection(RibbonMenu container)
+        {
+            return container.Items;
+        }
+
+    }
+}
