67955be651a9e19f8ac9955db0013cb4e9cc35d2
Philemon Eichin <phileicht@gmx.de>
2018-02-28 11:34:56 +0100

    Add ApplicationMenu RegionAdapter

---
+++ phirSOFT.FluentPrismAdapters/ApplicationMenuRegionAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fluent;
+using Prism.Regions;
+using phirSOFT.TopologicalComparison;
+
+namespace phirSOFT.FluentPrismAdapters
+{
+    /// <summary>
+    /// Provides a region adadpter dor an application menu.
+    /// </summary>
+    public class ApplicationMenuRegionAdapter : TopologicalSortedRegionAdapterBase<ApplicationMenu>
+    {
+        public ApplicationMenuRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory) : base(regionBehaviorFactory)
+        {
+        }
+
+        protected override IList GetItemCollection(ApplicationMenu container)
+        {
+            return container.Items;
+        }
+
+        public static void DeepMerge(ApplicationMenu target, ApplicationMenu source)
+        {
+            var comparer = GetComparer(target);
+            foreach (var sourceItem in source.Items)
+            {
+                if (comparer != null)
+                    target.Items.Insert(sourceItem, comparer);
+                else
+                    target.Items.Add(sourceItem);
+            }
+        }
+    }
+}
