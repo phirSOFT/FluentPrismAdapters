7cf55ec86060df8c15a8259ab6ec3547d62bec2c
Philemon Eichin <p.eichin@outlook.de>
2021-11-10 09:45:02 +0100

    Move annotations into own assembly

---
+++ phirSOFT.FluentPrismAdapters.Annotations/ContextualTabGroupAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phirSOFT.FluentPrismAdapters.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class ContextualTabGroupAttribute : Attribute
+    {
+        public Type ContextualTabGroupType { get; }
+        public string? ResolveName { get; }
+
+        public ContextualTabGroupAttribute(Type contextualTabGroupType, string? resolveName = null)
+        {
+            ContextualTabGroupType = contextualTabGroupType;
+            ResolveName = resolveName;
+        }
+    }
+}
